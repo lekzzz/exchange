628e8bd2b2c3d04d795ed3010f8ca024f9e8cf19
Alexey Rudanovsky <alexey.rudanovsky@factorymarket.com>
2018-08-22 18:08:11 +0300

    add text watchets

--- app/src/main/java/com/arudanovsky/exchange/CurrencyAdapter.kt
+++ app/src/main/java/com/arudanovsky/exchange/CurrencyAdapter.kt
@@ -2,11 +2,16 @@ package com.arudanovsky.exchange
 
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import io.reactivex.subjects.BehaviorSubject
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.ViewHolder>() {
 
@@ -29,6 +34,7 @@ class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.ViewHolder>() {
         }
 
     val clickSubject = BehaviorSubject.create<Int>()
+    val editableCurrenySubject = BehaviorSubject.create<Pair<String, BigDecimal>>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_currency, parent, false))
@@ -37,20 +43,46 @@ class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(holder.adapterPosition)
-        holder.itemView.setOnClickListener { clickSubject.onNext(holder.adapterPosition) }
-        holder.tvTitle.setOnClickListener { clickSubject.onNext(holder.adapterPosition) }
-        holder.etValue.setOnClickListener { clickSubject.onNext(holder.adapterPosition) }
+        val watcher = Watcher((items[holder.adapterPosition]))
+        holder.etValue.addTextChangedListener(watcher)
+        holder.etValue.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) clickSubject.onNext(holder.adapterPosition)
+            watcher.active = hasFocus
+        }
+        holder.itemView.setOnClickListener { holder.etValue.requestFocus() }
+        holder.tvTitle.setOnClickListener { holder.etValue.requestFocus() }
+    }
+
+    inner class Watcher(val currencyKey: String): TextWatcher {
+        var active = false
+        override fun afterTextChanged(s: Editable?) {
+            if (active) editableCurrenySubject.onNext(Pair(currencyKey, s.toBigDecimal()))
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 
     inner class ViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
         val etValue: TextView = itemView.findViewById(R.id.etValue)
-        private val currencyKey: String? = null
-        private var pos: Int? = null
+        private val rate: BigDecimal = Math.random().toBigDecimal()
+
+        init {
+            editableCurrenySubject
+                .subscribe {
+                    if (it.first != tvTitle.text)
+                        etValue.text = it.second.multiply(rate).setScale(2, RoundingMode.HALF_UP).toString()
+                }
+        }
 
         fun bind(position: Int) {
-            pos = position
+            Log.d("bind", "rate = $rate")
             tvTitle.text = items[position]
         }
     }
 }
+
+fun CharSequence?.toBigDecimal() =
+    if (isNullOrBlank()) BigDecimal.ZERO else BigDecimal(toString())
